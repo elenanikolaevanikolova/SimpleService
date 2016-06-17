eb59a98ae4d67435bf7826eedc1193f1df63b6c8
elena_nklv <elena_nklv@icloud.com>
2016-06-17 13:49:16 +0200

    simple service created

---
+++ SimpleService/ISimpleService.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace SimpleService
+{
+    [ServiceContract]
+    public interface ISimpleService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/MultiplyByTen/{number}",
+                           RequestFormat = WebMessageFormat.Xml,
+                           BodyStyle = WebMessageBodyStyle.Wrapped)]
+        int MultiplyByTen(string number);
+
+    }
+
+
+}
---
+++ SimpleService/SimpleService.svc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SimpleService
+{
+    public class SimpleService : ISimpleService
+    {
+        public int MultiplyByTen(string number)
+        {
+            var convertedNumber = Int32.Parse(number);
+            return 10 * convertedNumber;
+        }
+    }
+}
